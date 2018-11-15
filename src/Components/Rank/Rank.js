38e47a1b57f2851a693be80b044d12b2e2719a6f
santmato <santiago.mato.rpc@gmail.com>
2018-11-15 02:42:01 -0300

    Added Components

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+
+    return(
+        <div>
+            <p className='white f3'>
+                {'Your current Rank is..'}
+            </p>
+            <p className='white f1'>
+                {'#4'}
+            </p>
+        </div>
+    );
+}
+
+export default Rank;
