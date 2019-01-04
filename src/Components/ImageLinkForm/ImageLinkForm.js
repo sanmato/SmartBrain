6c7ab8d1220f940ce48ce45c5962e2829183665c
santmato <santiago.mato.rpc@gmail.com>
2019-01-04 17:49:54 -0300

    changing styles

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ const ImageLinkForm = ({onInputChange, onSubmitButton}) => {
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
                     <input className='center f4 pa2 w-70 center' type="tex" onChange={onInputChange}/>
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onSubmitButton}>Detect</button>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-green' onClick={onSubmitButton}>Detect</button>
                 </div>
             </div>
         </div>
